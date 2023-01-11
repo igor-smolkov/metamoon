8514fb5d56a14b28b29ca81d60c04b71a2a256c0
Igor Smolkov <smolkov.igor@gmail.com>
2023-01-11 19:22:43 +0530

    Add ConnectButtonPanel, extened Button

--- src/modules/Home/components/ConnectWalletPanel/index.tsx
+++ src/modules/Home/components/ConnectWalletPanel/index.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react';
+import NextImage from 'next/image';
+import { Montserrat } from '@next/font/google';
+import cn from 'classnames';
 
 import { Button } from 'shared/components';
 import styles from './connectWalletPanel.module.scss';
 
+const montserrat = Montserrat({ subsets: ['latin', 'cyrillic'], weight: ['400', '500'], variable: '--font-main' });
+
 const ConnectWalletPanel: FC = () => (
-  <section className={styles.root}>
+  <section className={cn(styles.root, montserrat.variable)}>
     <h2 className={styles.title}>Connect wallet</h2>
     <div className={styles.buttons}>
-      <Button>Metamask</Button>
-      <Button>WalletConnect</Button>
-      <Button>Coinbase Wallet</Button>
+      <Button theme="group">
+        <NextImage src="/images/metamask.svg" width={24} height={22} alt="Metamask" /> Metamask
+      </Button>
+      <Button theme="group">
+        <NextImage src="/images/metamask.svg" width={24} height={22} alt="Metamask" /> WalletConnect
+      </Button>
+      <Button theme="group">
+        <NextImage src="/images/metamask.svg" width={24} height={22} alt="Metamask" /> Coinbase Wallet
+      </Button>
     </div>
   </section>
 );
--- src/shared/components/Button/index.tsx
+++ src/shared/components/Button/index.tsx
@@ -1,13 +1,15 @@
 import { FC, PropsWithChildren } from 'react';
+import cn from 'classnames';
 
 import styles from './button.module.scss';
 
 type Props = {
+  theme?: 'text' | 'group';
   onClick?: () => void;
 };
 
-const Button: FC<PropsWithChildren<Props>> = ({ children, onClick }) => (
-  <button className={styles.root} type="button" onClick={onClick}>
+const Button: FC<PropsWithChildren<Props>> = ({ children, theme = 'text', onClick }) => (
+  <button className={cn(styles.root, { [styles.themeGroup]: theme === 'group' })} type="button" onClick={onClick}>
     {children}
   </button>
 );
